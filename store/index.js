c4c42c4d83d711621222ab880e01d870c4b5679d
MACEDO <macedo.gti@gmail.com>
2020-09-27 18:09:47 -0300

    Delete comments in index store

--- store/index.js
+++ store/index.js
@@ -4,24 +4,22 @@ import Vuex from 'vuex'
 // Vue.use(Vuex)
 
 new Vuex.Store({
-  state: {
-    user: null,
-    // item: {},
-  },
-  mutations: {
-    // setItem(state, obj) {
-    //   state.item = obj
-    // },
-
-
-    setUser(state, user) {
-      state.user = user
-      if (user) {
-        axios.defaults.headers.common['Authorization'] = `bearer ${user.token}`
-      } else {
-        delete axios.defaults.headers.common['Authorization']
-      }
+    state: {
+        user: {},
+        // item: {},
     },
+    mutations: {
+        setUser(state, user) {
+            state.user = user
+            if (user) {
+                axios.defaults.headers.common['Authorization'] = `bearer ${user.token}`
+            } else {
+                delete axios.defaults.headers.common['Authorization']
+            }
+        },
+        // setItem(state, obj) {
+        //   state.item = obj
+        // },
 
-  }
+    }
 })
