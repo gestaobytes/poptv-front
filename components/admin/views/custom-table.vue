0c923cbcec89bfc87ed0d4cba4e4a92525672e28
MACEDO <macedo.gti@gmail.com>
2020-09-29 07:54:40 -0300

    Delete comments

--- components/admin/views/custom-table.vue
+++ components/admin/views/custom-table.vue
@@ -17,20 +17,3 @@ export default {
   name: "CustomTable"
 };
 </script>
-
- <!-- 
-  props: ["widthTdButtonsController", "headTH", "registers"],
- <v-simple-table class="elevation-3" dense>
-    <thead>
-        <th v-for="head in headTH" :key="head" :width="head[1]"><h3 class="cyan--text">{{head[0]}}</h3></th>
-        <th :width="widthTdButtonsController"></th>
-    </thead>
-    <tbody>
-      <tr v-for="data in registers" :key="data.id">
-        <td v-for="i in data" :key="i.id" v-html="i"></td> 
-        <td class="text-right">
-          <slot name="buttonsController"></slot>
-        </td>
-      </tr>
-    </tbody>
-  </v-simple-table>-->
