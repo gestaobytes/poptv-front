895c7d51ad4ceb5e778fd35d65877d2e23220ae9
MACEDO <macedo.gti@gmail.com>
2020-09-29 07:54:05 -0300

    Correction in store

--- store/index.js
+++ store/index.js
@@ -1,25 +1,17 @@
 import axios from 'axios'
 import Vuex from 'vuex'
 
-// Vue.use(Vuex)
-
-new Vuex.Store({
-    state: {
-        user: {},
-        // item: {},
-    },
-    mutations: {
-        setUser(state, user) {
-            state.user = user
-            if (user) {
-                axios.defaults.headers.common['Authorization'] = `bearer ${user.token}`
-            } else {
-                delete axios.defaults.headers.common['Authorization']
-            }
-        },
-        // setItem(state, obj) {
-        //   state.item = obj
-        // },
-
-    }
+export const state = () => ({
+    user: {}
 })
+
+export const mutations = {
+    setUser(state, user) {
+        state.user = user
+        if (user) {
+            axios.defaults.headers.common['Authorization'] = `bearer ${user.token}`
+        } else {
+            delete axios.defaults.headers.common['Authorization']
+        }
+    },
+}
